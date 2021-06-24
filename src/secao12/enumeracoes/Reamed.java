049bf11af8ee3311bce3782f8cb1ade9050a0e11
RobsonAlmeida <robsonalmeida073@gmail.com>
2021-06-24 00:02:21 -0300

    secao 12 - adicionado Reamed

---
+++ src/secao12/enumeracoes/Reamed.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secao12.enumeracoes;
+
+/**
+ *
+ * @author robson-pc
+ */
+public class Reamed {
+    //Checklist
+    /*
+    Define / discussão
+    Exemplo: estados de um pedido
+    Conversão de string para enum
+    Representação UML
+    */
+}
